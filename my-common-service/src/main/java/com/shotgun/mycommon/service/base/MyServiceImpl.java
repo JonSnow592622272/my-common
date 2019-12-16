e7abff2baabd52c95e1f5157991c796fd1304976
wulingming <wulingming@zhixueyun.com>
2019-12-16 20:16:13 +0800

    修正编码格式

--- my-common-service/src/main/java/com/shotgun/mycommon/service/base/MyServiceImpl.java
+++ my-common-service/src/main/java/com/shotgun/mycommon/service/base/MyServiceImpl.java
@@ -5,10 +5,12 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.shotgun.mycommon.base.util.JacksonUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 
 public class MyServiceImpl<M extends BaseMapper<T>, T> extends ServiceImpl<M, T> implements MyIService<T> {
 
 
+    @GetMapping("/testGet10")
     @Override
     public IPage<T> testGet10() {
         IPage<T> page = page(new Page<>(1, 10));
