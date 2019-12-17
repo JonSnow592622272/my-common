98f96f48a3834f12fe4241a09e013038469ea109
wulingming <wulingming@zhixueyun.com>
2019-12-17 21:05:35 +0800

    修正

--- my-common-service/src/main/java/com/shotgun/mycommon/service/base/MyIService.java
+++ my-common-service/src/main/java/com/shotgun/mycommon/service/base/MyIService.java
@@ -1,11 +1,11 @@
 package com.shotgun.mycommon.service.base;
 
-import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 public interface MyIService<T> extends IService<T> {
 
-    IPage<T> testGet10();
+    Page<T> testGet10();
 
 
 }
--- my-common-service/src/main/java/com/shotgun/mycommon/service/base/MyServiceImpl.java
+++ my-common-service/src/main/java/com/shotgun/mycommon/service/base/MyServiceImpl.java
@@ -1,6 +1,5 @@
 package com.shotgun.mycommon.service.base;
 
-import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.shotgun.mycommon.base.util.JacksonUtils;
@@ -9,8 +8,8 @@ public class MyServiceImpl<M extends MyBaseMapper<T>, T> extends ServiceImpl<M, 
 
 
     @Override
-    public IPage<T> testGet10() {
-        IPage<T> page = page(new Page<>(1, 10));
+    public Page<T> testGet10() {
+        Page<T> page = (Page<T>) page(new Page<>(1, 10));
         System.out.println("testGet10:::" + JacksonUtils.writeValueAsString(page));
         return page;
     }
