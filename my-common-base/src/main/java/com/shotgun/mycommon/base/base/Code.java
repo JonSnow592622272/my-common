03d5927d0941b053d2a6599e14e40757c1ca4752
wulingming <wulingming@zhixueyun.com>
2020-05-19 15:16:09 +0800

    完善

--- my-common-base/src/main/java/com/shotgun/mycommon/base/base/Code.java
+++ my-common-base/src/main/java/com/shotgun/mycommon/base/base/Code.java
@@ -3,6 +3,8 @@ package com.shotgun.mycommon.base.base;
 
 public interface Code {
 
+    String getSystemCode();
+
     int getValue();
 
     String getText();
--- my-common-base/src/main/java/com/shotgun/mycommon/base/base/ResultInfo.java
+++ my-common-base/src/main/java/com/shotgun/mycommon/base/base/ResultInfo.java
@@ -1,45 +1,79 @@
 package com.shotgun.mycommon.base.base;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ResultInfo<E> implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	/** 系统编码 */
-	private String systemCode;
+    /**
+     * 状态码分界线，大于该值的所有状态都表示错误
+     */
+    private static final int STATUS_CODE_DIVIDING = 1;
 
-	/** 状态码 */
-	private Integer statusCode;
+    /**
+     * 系统编码
+     */
+    private String systemCode;
 
-	/** 返回提示信息 */
-	private String msg;
+    /**
+     * 状态码
+     */
+    private Integer statusCode;
 
-	/** 返回数据 */
-	private E data;
+    /**
+     * 返回提示信息
+     */
+    private String msg;
 
-	private ResultInfo() {
-	}
+    /**
+     * 返回数据
+     */
+    private E data;
 
-    public ResultInfo(String systemCode, Code code) {
-        this.systemCode = systemCode;
+    private ResultInfo() {
+    }
+
+    public ResultInfo(Code... errorCodes) {
+        this.systemCode = errorCodes[0].getSystemCode();
+        this.statusCode = STATUS_CODE_DIVIDING;
+        this.msg = "操作成功";
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("systemCodes", Stream.of(errorCodes).map(Code::getSystemCode).collect(Collectors.toList()));
+        data.put("statusCodes", Stream.of(errorCodes).map(Code::getValue).collect(Collectors.toList()));
+        data.put("msgs", Stream.of(errorCodes).map(Code::getText).collect(Collectors.toList()));
+
+        this.data = (E) data;
+    }
+
+    public ResultInfo(Code code) {
+        this.systemCode = code.getSystemCode();
         this.statusCode = code.getValue();
         this.msg = code.getText();
     }
 
-    public ResultInfo(String systemCode, Code code, E data) {
-        this.systemCode = systemCode;
+    public ResultInfo(Code code, E data) {
+        this.systemCode = code.getSystemCode();
         this.statusCode = code.getValue();
         this.msg = code.getText();
         this.data = data;
     }
 
     /*是否失败*/
+    @JsonIgnore
     public boolean isFailed() {
-        return this.statusCode > 0;
+        return this.statusCode > STATUS_CODE_DIVIDING;
     }
 
     /*是否成功*/
+    @JsonIgnore
     public boolean isSuccess() {
         return !isFailed();
     }
@@ -75,4 +109,5 @@ public class ResultInfo<E> implements Serializable {
     public void setData(E data) {
         this.data = data;
     }
+
 }
