37f9077d1cb17ab32cbb198f171412664014f7ed
wulingming <wulingming@zhixueyun.com>
2020-05-19 16:55:50 +0800

    完善

--- my-common-service/src/main/java/com/shotgun/mycommon/service/base/MyIService.java
+++ my-common-service/src/main/java/com/shotgun/mycommon/service/base/MyIService.java
@@ -1,36 +1,67 @@
 package com.shotgun.mycommon.service.base;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.shotgun.mycommon.base.base.Code;
 import com.shotgun.mycommon.base.base.ResultInfo;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 
 /**
  * @author wulm
  **/
 public interface MyIService<T> /*extends IService<T> 这里注释掉是为了不暴露给其他service使用，避免其他service实现类调用里面的方法来绕过逻辑验证*/ {
+    Code SUCCESS_CODE = new Code() {
+        @Override
+        public String getSystemCode() {
+            return "common";
+        }
+
+        @Override
+        public int getValue() {
+            return 0;
+        }
+
+        @Override
+        public String getText() {
+            return "";
+        }
+    };
 
     IPage<T> testGet10();
 
     /**
      * 单条插入
      **/
-    ResultInfo insert(T record);
+    default ResultInfo insert(T record) {
+        return insertBatch(Collections.singletonList(record));
+    }
 
     /**
      * 批量插入
      **/
-    ResultInfo insertBatch(T... records);
+    @Transactional(rollbackFor = Exception.class)
+    default ResultInfo insertBatch(T... records) {
+        return insertBatch(Arrays.asList(records));
+    }
 
     /**
      * 批量插入
      **/
-    ResultInfo insertBatch(Collection<T> records);
+    @Transactional(rollbackFor = Exception.class)
+    default ResultInfo insertBatch(Collection<T> records) {
+        return insertBatchUsePage(1000, records);
+    }
 
     /**
      * 分页批量插入
      **/
-    ResultInfo insertBatchUsePage(int batchSize, T... records);
+    @Transactional(rollbackFor = Exception.class)
+    default ResultInfo insertBatchUsePage(int batchSize, T... records) {
+        return insertBatchUsePage(batchSize, Arrays.asList(records));
+    }
 
     /**
      * 分页批量插入
--- my-common-service/src/main/java/com/shotgun/mycommon/service/base/MyServiceImpl.java
+++ my-common-service/src/main/java/com/shotgun/mycommon/service/base/MyServiceImpl.java
@@ -5,16 +5,17 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.shotgun.mycommon.base.base.ResultInfo;
 import com.shotgun.mycommon.base.util.JacksonUtils;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 
 /**
  * @author wulm
  * 所有serviceImpl的基类，用于存放公共基础方法
  **/
 public class MyServiceImpl<M extends MyBaseMapper<T>, T> extends ServiceImpl<M, T> implements MyIService<T> {
+
+
 
     //消息一部：对象json转给消息中间件，消息消费调用原提供接口方（缺点：跨数据源多步操作无事务控制）
 
@@ -26,32 +27,12 @@ public class MyServiceImpl<M extends MyBaseMapper<T>, T> extends ServiceImpl<M, 
         return page;
     }
 
-    @Override
-    public ResultInfo insert(T record) {
-        return insertBatch(Collections.singletonList(record));
-    }
 
-    @Override
-    public ResultInfo insertBatch(T... records) {
-        return insertBatch(Arrays.asList(records));
-    }
-
-    @Override
-    public ResultInfo insertBatch(Collection<T> records) {
-        return insertBatchUsePage(1000, records);
-    }
-
-    @Override
-    public ResultInfo insertBatchUsePage(int batchSize, T... records) {
-        return insertBatchUsePage(batchSize, Arrays.asList(records));
-    }
-
+    @Transactional(rollbackFor = Exception.class)
     @Override
     public ResultInfo insertBatchUsePage(int batchSize, Collection<T> records) {
-
         saveBatch(records, batchSize);
-
-        return null;
+        return new ResultInfo<>(SUCCESS_CODE);
     }
 
 }
